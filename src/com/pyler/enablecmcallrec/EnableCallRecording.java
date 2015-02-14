79b8de44bcc8e79c7f3a77f1be966672842520ec
pylerSM <pyler@azet.sk>
2015-02-14 11:22:57 +0100

    Open source module

---
+++ src/com/pyler/enablecmcallrec/EnableCallRecording.java
@@ -0,0 +1,37 @@
+package com.pyler.enablecmcallrec;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import android.content.Context;
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class EnableCallRecording implements IXposedHookLoadPackage,
+		IXposedHookInitPackageResources {
+	public static final String DIALER = "com.android.dialer";
+
+	@Override
+	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+		if (!DIALER.equals(lpparam.packageName)) {
+			return;
+		}
+		findAndHookMethod(
+				"com.android.services.callrecorder.CallRecorderService",
+				lpparam.classLoader, "isEnabled", Context.class,
+				XC_MethodReplacement.returnConstant(true));
+
+	}
+
+	@Override
+	public void handleInitPackageResources(InitPackageResourcesParam resparam)
+			throws Throwable {
+		if (!DIALER.equals(resparam.packageName)) {
+			return;
+		}
+		resparam.res.setReplacement(DIALER, "bool", "call_recording_enabled",
+				true);
+	}
+
+}
