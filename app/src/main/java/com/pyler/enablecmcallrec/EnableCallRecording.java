ddb618b8709e36925d2484d9e992b95b6803b0d1
pyler <pyler@azet.sk>
2016-07-12 17:28:52 +0200

    Updated

--- app/src/main/java/com/pyler/enablecmcallrec/EnableCallRecording.java
+++ app/src/main/java/com/pyler/enablecmcallrec/EnableCallRecording.java
@@ -1,36 +1,38 @@
 package com.pyler.enablecmcallrec;
 
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import android.content.Context;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
 public class EnableCallRecording implements IXposedHookLoadPackage,
-		IXposedHookInitPackageResources {
-	public static final String DIALER = "com.android.dialer";
-	public static final String CALL_RECORDING_SERVICE = "com.android.services.callrecorder.CallRecorderService";
+        IXposedHookInitPackageResources {
+    public static final String DIALER = "com.android.dialer";
+    public static final String CALL_RECORDING_SERVICE = "com.android.services.callrecorder.CallRecorderService";
 
-	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		if (!DIALER.equals(lpparam.packageName)) {
-			return;
-		}
-		findAndHookMethod(CALL_RECORDING_SERVICE, lpparam.classLoader,
-				"isEnabled", Context.class,
-				XC_MethodReplacement.returnConstant(true));
-	}
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if (!DIALER.equals(lpparam.packageName)) {
+            return;
+        }
+        findAndHookMethod(CALL_RECORDING_SERVICE, lpparam.classLoader,
+                "isEnabled", Context.class,
+                XC_MethodReplacement.returnConstant(true));
+    }
 
-	@Override
-	public void handleInitPackageResources(InitPackageResourcesParam resparam)
-			throws Throwable {
-		if (!DIALER.equals(resparam.packageName)) {
-			return;
-		}
-		resparam.res.setReplacement(DIALER, "bool", "call_recording_enabled",
-				true);
-	}
+    @Override
+    public void handleInitPackageResources(InitPackageResourcesParam resparam)
+            throws Throwable {
+        if (!DIALER.equals(resparam.packageName)) {
+            return;
+        }
+        resparam.res.setReplacement(DIALER, "bool", "call_recording_enabled",
+                true);
+    }
 
 }	
